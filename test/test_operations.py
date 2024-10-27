a99510d08fb09601144330508360786fab5ee4b6
“gdeotale” <gdeotale@gmail.com>
2024-10-27 17:18:37 +0530

    'wokflow1'

--- test/test_operations.py
+++ test/test_operations.py
@@ -1,12 +1,12 @@
 from src.math_operations import add, sub
 
-def test_add(a,b):
+def test_add():
     assert add(2,3) == 5
     assert add(-2,3) == 1
     assert add(2,-3) == -1
     assert add(-2,-3) == -5
 
-def test_sub(a,b):
+def test_sub():
     assert sub(2,3) == -1
     assert sub(-2,3) == -5
     assert sub(2,-3) == 5
